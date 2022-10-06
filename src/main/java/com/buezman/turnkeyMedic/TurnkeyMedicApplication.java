2e954c8dc715f646c53c9bb57a4dacb101270fd5
Buezman <54446003+Buezman@users.noreply.github.com>
2022-10-06 05:25:40 +0100

    first commit

---
+++ src/main/java/com/buezman/turnkeyMedic/TurnkeyMedicApplication.java
@@ -0,0 +1,13 @@
+package com.buezman.turnkeyMedic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnkeyMedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnkeyMedicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buezman/turnkeyMedic/TurnkeyMedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buezman.turnkeyMedic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnkeyMedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
